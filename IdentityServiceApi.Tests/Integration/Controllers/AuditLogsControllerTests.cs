a4755e608ebf1090623bec5673eb672807e2d1f8
cbriggs02 <chris.briglio@gmail.com>
2025-03-31 17:30:04 -0400

    Created helper class for JWT's used in tests and completed all tests for audit logger controller HTTP/HTTPS scenarios

---
+++ IdentityServiceApi.Tests/Integration/Controllers/AuditLogsControllerTests.cs
@@ -0,0 +1,130 @@
+﻿using IdentityServiceApi.Controllers;
+using IdentityServiceApi.Models.ApiResponseModels.AuditLogsResponses;
+using System.Net.Http.Headers;
+using System.Net;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc.Testing;
+using IdentityServiceApi.Tests.Integration.Helpers;
+using IdentityServiceApi.Constants;
+
+namespace IdentityServiceApi.Tests.Integration.Controllers
+{
+    /// <summary>
+    ///     Unit tests for the <see cref="AuditLogsController"/> class.
+    ///     This class contains test cases for various audit logs controller HTTP/HTTPS scenarios, verifying the 
+    ///     behavior of the audit logs controller functionality.
+    /// </summary>
+    /// <remarks>
+    ///     @Author: Christian Briglio
+    ///     @Created: 2025
+    /// </remarks>
+    [Trait("TestCategory", "IntegrationTest")]
+    public class AuditLogsControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _client;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="AuditLogsControllerTests"/> class.
+        ///     This constructor sets up the HTTP client for testing using the provided <see cref="WebApplicationFactory{TEntryPoint}"/>.
+        /// </summary>
+        /// <param name="factory">
+        ///     The web application factory used to create the client for integration testing.
+        /// </param>
+        public AuditLogsControllerTests(WebApplicationFactory<Program> factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        /// <summary>
+        ///     Verifies that the <see cref="AuditLogsController.GetLogs"/> method returns an OK response (200) 
+        ///     when the user is authorized and audit logs data exists.
+        /// </summary>
+        /// <returns>
+        ///     A task that represents the asynchronous unit test operation.
+        /// </returns>
+        [Fact]
+        public async Task GetLogs_ReturnsOk_WhenAuthorizedAndDataExists()
+        {
+            // Arrange
+            AuthenticateClient();
+            var requestUri = "/api/v1/AuditLogs?Page=1&PageSize=5&Action=0";
+
+            // Act
+            var response = await _client.GetAsync(requestUri);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            var content = await response.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<AuditLogListResponse>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+            Assert.NotNull(result);
+            Assert.NotEmpty(result.Logs);
+        }
+
+        /// <summary>
+        ///     Verifies that the <see cref="AuditLogsController.GetLogs"/> method returns an Unauthorized response (401) 
+        ///     when the user is not authenticated.
+        /// </summary>
+        /// <returns>
+        ///     A task that represents the asynchronous unit test operation.
+        /// </returns>
+        [Fact]
+        public async Task GetLogs_ReturnsUnauthorized_WhenNotAuthenticated()
+        {
+            // Arrange
+            var requestUri = "/api/v1/AuditLogs?PageNumber=1&PageSize=5&AuditLogs?Page=1&PageSize=5&Action=0";
+
+            // Act
+            var response = await _client.GetAsync(requestUri);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+        }
+
+        /// <summary>
+        ///     Verifies that the <see cref="AuditLogsController.DeleteLog"/> method returns a NotFound response (404) 
+        ///     when trying to delete a log that does not exist.
+        /// </summary>
+        /// <returns>
+        ///     A task that represents the asynchronous unit test operation.
+        /// </returns>
+        [Fact]
+        public async Task DeleteLog_ReturnsNotFound_WhenLogDoesNotExist()
+        {
+            // Arrange
+            AuthenticateClient();
+            var requestUri = "/api/v1/AuditLogs/nonexistent-log-id";
+
+            // Act
+            var response = await _client.DeleteAsync(requestUri);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        /// <summary>
+        ///     Verifies that the <see cref="AuditLogsController.DeleteLog"/> method returns an Unauthorized response (401) 
+        ///     when the user is not authenticated.
+        /// </summary>
+        /// <returns>
+        ///     A task that represents the asynchronous unit test operation.
+        /// </returns>
+        [Fact]
+        public async Task DeleteLog_ReturnsUnauthorized_WhenNotAuthenticated()
+        {
+            // Arrange
+            var requestUri = "/api/v1/AuditLogs/valid-log-id";
+
+            // Act
+            var response = await _client.DeleteAsync(requestUri);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+        }
+
+        private void AuthenticateClient()
+        {
+            var token = JwtTokenTestHelper.GenerateJwtToken(roles: new List<string> { Roles.SuperAdmin });
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        }
+    }
+}
---
+++ IdentityServiceApi.Tests/Integration/Helpers/JwTokenTestHelper.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace IdentityServiceApi.Tests.Integration.Helpers
+{
+    /// <summary>
+    ///     Helper class for generating JWT tokens for testing purposes.
+    ///     This class uses an in-memory configuration to generate a token with customizable roles.
+    /// </summary>
+    /// <remarks>
+    ///     @Author: Christian Briglio
+    ///     @Created: 2025
+    /// </remarks>
+    public static class JwtTokenTestHelper
+    {
+        private static IConfiguration Configuration { get; } = new ConfigurationBuilder()
+            .AddInMemoryCollection(new Dictionary<string, string>
+            {
+                {"JwtSettings:ValidIssuer", "https://localhost:7234"},
+                {"JwtSettings:ValidAudience", "https://localhost:3000"},
+                {"JwtSettings:SecretKey", "NjwQN0JTxC1^Rd5VEFf&&@e$$q4B0BaR^y8Q%1t&!FHo)a%@w&1pi)WJKjbaMVlk"}
+            })
+            .Build();
+
+        /// <summary>
+        ///     Generates a JWT token with a specified list of roles for testing.
+        /// </summary>
+        /// <param name="roles">
+        ///     A list of roles to include in the generated JWT token. Can be null or empty.
+        /// </param>
+        /// <returns>
+        ///     A JWT token string that can be used for authentication in integration tests.
+        /// </returns>
+        public static string GenerateJwtToken(IList<string> roles)
+        {
+            const string userId = "test-user-id";
+            const string userName = "test-user";
+
+            var validIssuer = Configuration["JwtSettings:ValidIssuer"];
+            var validAudience = Configuration["JwtSettings:ValidAudience"];
+            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JwtSettings:SecretKey"]));
+            var signingCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new List<Claim>
+            {
+                new(ClaimTypes.NameIdentifier, userId),
+                new(ClaimTypes.Name, userName),
+            };
+
+            if (roles != null && roles.Any())
+            {
+                foreach (var role in roles)
+                {
+                    claims.Add(new Claim(ClaimTypes.Role, role));
+                }
+            }
+
+            var tokenOptions = new JwtSecurityToken(
+                issuer: validIssuer,
+                audience: validAudience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddHours(1),
+                signingCredentials: signingCredentials
+            );
+
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+            return tokenString;
+        }
+    }
+}
