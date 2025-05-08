d81e563dec3cb4571b04c3f3a7a9616143fca21b
cbriggs02 <chris.briglio@gmail.com>
2025-05-08 14:37:17 -0400

    Added xml doc for authentication service ext

--- IdentityServiceApi/Extensions/AuthenticationExtensions.cs
+++ IdentityServiceApi/Extensions/AuthenticationExtensions.cs
@@ -6,7 +6,7 @@ using System.Text;
 namespace IdentityServiceApi.Extensions
 {
     /// <summary>
-    /// 
+    ///     Provides extension methods to configure JWT authentication services for the IdentityServiceApi.
     /// </summary>
     /// <remarks>
     ///     @Author: Christian Briglio
@@ -15,12 +15,20 @@ namespace IdentityServiceApi.Extensions
     public static class AuthenticationExtensions
     {
         /// <summary>
-        /// 
+        ///     Adds JWT authentication to the service collection using settings from the application's configuration.
         /// </summary>
-        /// <param name="services"></param>
-        /// <param name="configuration"></param>
-        /// <returns></returns>
-        /// <exception cref="InvalidOperationException"></exception>
+        /// <param name="services">The <see cref="IServiceCollection"/>
+        ///     to which the authentication services will be added.
+        /// </param>
+        /// <param name="configuration">
+        ///     The <see cref="IConfiguration"/> instance used to retrieve JWT settings.
+        /// </param>
+        /// <returns>
+        ///     The same <see cref="IServiceCollection"/> instance to allow method chaining.
+        /// </returns>
+        /// <exception cref="InvalidOperationException">
+        ///     Thrown if the <c>JwtSettings</c> section is missing or if the secret key is less than 32 bytes in length.
+        /// </exception>
         public static IServiceCollection AddAuthenticationServices(this IServiceCollection services, IConfiguration configuration)
         {
             services.Configure<JwtSettings>(configuration.GetSection("JwtSettings"));
